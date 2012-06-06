6163e6a1a3cfeadbbbef9436d3a9cae19eea07fd
Amanda Martinez <amandamartinez@amanda-martinezs-imac.local>
2012-06-06 15:30:24 -0600

    Exercises 1-4  Please enter the commit message for your changes. Lines starting

---
+++ ex3/ex3.py
@@ -0,0 +1,35 @@
+# This will print the words below
+print "I will now count my chickens:"
+
+# This will calculate the Hens and Roosters
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+# This will print the words below
+print "Now I will count the eggs:"
+
+# This will print the calculation below
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# This will print the words below
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# This will print the calculation below
+print 3 + 2 < 5 - 7
+
+# This will print the question below and then the calculation
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# This will print the words below
+print "Oh, that's why it's False."
+
+# This will print the words below
+print "How about some more."
+
+# This will print the question below and answer
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+print 10.0 / 3.0
