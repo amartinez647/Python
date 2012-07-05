5bd300c396f0e23b1b924cc2fac31f5feff40ca6
Amanda Martinez <amandamartinez@amanda-martinezs-imac.local>
2012-07-05 14:08:43 -0600

    Exercises 10-17  Please enter the commit message for your changes. Lines starting

---
+++ ex11/ex11-extra.py
@@ -0,0 +1,8 @@
+print "What grade are you in?",
+grade = raw_input()
+print "What's your favorite subject?",
+subject = raw_input()
+print "Who's your favorite teacher?",
+teacher = raw_input ()
+
+print "You're in %r grade, your favorite subject is %r, and your favorite teacher is %r." % (grade, subject, teacher)
