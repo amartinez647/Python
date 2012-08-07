9b3a50636aa17cc68d41e242aa6c02902c780360
Amanda Martinez <amandamartinez@amanda-martinezs-imac.local>
2012-08-07 13:21:52 -0600

    Exercise 36 updated

--- ex36/ex36.py
+++ ex36/ex36.py
@@ -1,64 +1,62 @@
 from sys import exit
 
-def pink_room():
-    print "Here's the pink room. Do you like it?"
+
+
+def right_tunnel():
+    print "There is a herd of sheep blocking the road."
+    print "Do you honk at the sheep, run over the sheep, or play with the sheep?"
+    sheep_moved = False
+
+    while True:
+        next = raw_input("> ")
+
+        if next == "run over":
+            dead("How horrible!!")
+        elif next == "honk" and not sheep_moved:
+            print "The sheep baa really loud, but move out of the way."
+            sheep_moved = True
+            exit(0)
+        elif next == "honk" and sheep_moved:
+            dead("Well...maybe you should try something else genius!")
+        elif next == "play":
+            dead("Great! You live happily ever after with the sheep!")
+        else:
+            print "That's not an option!"
+
+
+def middle_tunnel():
+    print "You are starving and come across an apple tree."
+    print "Do you keep driving or stop to eat some apples?"
 
     next = raw_input("> ")
-    if "Yes" in next:
-        print "Awesome!"
 
-    elif "No" in next:
-        print "Bummer!"
+    if "keep driving" in next:
         exit(0)
+    elif "apples" in next:
+        dead("Those were some good apples!")
     else:
-        dead("Goodbye!")
-        
-        
-def purple_room():
-    print "Here's the purple room. Do you like it?"
-
-    next = raw_input("> ")
-    if "Yes" in next:
-        print "Awesome!"
-
-    elif "No" in next:
-        print "Bummer!"
-        exit(0)
-    else:
-        dead("Goodbye!")
-        
-        
-def blue_room():
-    print "Here's the blue room. Do you like it?"
-
-    next = raw_input("> ")
-    if "Yes" in next:
-        print "Awesome!"
-
-    elif "No" in next:
-        print "Bummer!"
-        exit(0)
-    else:
-        dead("Goodbye!")
+        start()
 
 
 def dead(why):
-    print why, "Goodbye!"
+    print why, "Well...better luck next time!"
     exit(0)
-    
+
 def start():
-    print "What is your favorite color? Pink, purple or blue?"
+    print "You are driving down a long windy road."
+    print "You come to three tunnels."
+    print "Do you choose to go through the one to the left, middle, or right?"
 
     next = raw_input("> ")
 
-    if next == "pink" or "Pink":
-        pink_room()
-    elif next == "purple" or "Purple":
-        purple_room()
-    elif next == "blue" or "Blue":
-        blue_room()
+    if next == "right":
+        right_tunnel()
+    elif next == "middle":
+        middle_tunnel()
+    elif next == "left":
+        dead("That tunnel drops you into a black hole!")
     else:
-        dead("You seriously need color in your life.")
-        
-        
+        dead("You only had three choices...left, middle or right!")
+
+
 start()
